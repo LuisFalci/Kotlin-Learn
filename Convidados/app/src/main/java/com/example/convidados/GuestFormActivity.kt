147177a266cde89470bd2593a5ecfc228f4f6482
Luis Gustavo Furtuozo Falci <luisgustavo.1999@hotmail.com>
2023-08-12 02:05:59 -0300

    Cadastro atualizado

--- Convidados/app/src/main/java/com/example/convidados/GuestFormActivity.kt
+++ Convidados/app/src/main/java/com/example/convidados/GuestFormActivity.kt
@@ -2,10 +2,31 @@ package com.example.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(),View.OnClickListener {
+
+    // lateinit inicializa a variável futuramente, assim não tem necessidade de passar valor
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var  viewModel: GuestFormViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Instanciamos a viewModel, atrelando ela ao ciclo de vida. Assim, quando morre a activity, morre a viewmodel também
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresence.isChecked = true
+    }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.button_save){
+
+        }
     }
 }
