baefcfad092c32f88fd7c06e0f70afd71214d312
Luis Gustavo Furtuozo Falci <luisgustavo.1999@hotmail.com>
2023-08-14 16:54:00 -0300

    Listagem convidados RecyclerView Parte 1

--- Convidados/app/src/main/java/com/example/convidados/viewmodel/AllGuestsViewModel.kt
+++ Convidados/app/src/main/java/com/example/convidados/viewmodel/AllGuestsViewModel.kt
@@ -1,13 +1,21 @@
 package com.example.convidados.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.convidados.model.GuestModel
+import com.example.convidados.repository.GuestRepository
 
-class AllGuestsViewModel : ViewModel() {
+class AllGuestsViewModel(application: Application) : AndroidViewModel(application) {
+    //instancia uma vez para não precisar instanciar toda vez que eu chamar uma função
+    private val repository = GuestRepository.getInstance(application.applicationContext)
+    // Estou observando uma lista de guestModel
+    private val listAllGuests = MutableLiveData<List<GuestModel>>()
+    val guests: LiveData<List<GuestModel>> = listAllGuests
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    fun getAll(){
+    listAllGuests.value = repository.getAll()
     }
-    val text: LiveData<String> = _text
 }
